95e45cefb41fc9a07791362036c111ddac550eff
junta <79247735+junoota0115@users.noreply.github.com>
2022-10-13 18:05:13 +0900

    テストコード　結合

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,12 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    context '新規登録できる時' do
     it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
       expect(@user).to be_valid
     end
+  end
+  context '新規登録できないとき' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -59,4 +62,5 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
     end
   end
+end
 end
